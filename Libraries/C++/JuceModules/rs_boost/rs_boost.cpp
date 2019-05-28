f575233712b812712c33cd59637b45c10a691221
Robin Schmidt <robin@rs-met.com>
2019-05-28 19:26:38 +0200

    added more cpp includes to unity build file rs_boost.cpp

--- Libraries/C++/JuceModules/rs_boost/rs_boost.cpp
+++ Libraries/C++/JuceModules/rs_boost/rs_boost.cpp
@@ -21,3 +21,33 @@ header files that the compiler may be using. */
 #include "libs//python/src/str.cpp"
 #include "libs//python/src/tuple.cpp"
 #include "libs//python/src/wrapper.cpp"
+
+#include "libs//python/src/converter/arg_to_python_base.cpp"
+//#include "libs//python/src/converter/builtin_converters.cpp"
+//#include "libs//python/src/converter/from_python.cpp"
+//#include "libs//python/src/converter/registry.cpp"
+//#include "libs//python/src/converter/type_id.cpp"
+
+/*
+// does not work yet - apparently, we also need the numpy path to the include path...
+#include "libs//python/src/numpy/dtype.cpp"
+#include "libs//python/src/numpy/matrix.cpp"
+#include "libs//python/src/numpy/ndarray.cpp"
+#include "libs//python/src/numpy/numpy.cpp"
+#include "libs//python/src/numpy/scalars.cpp"
+#include "libs//python/src/numpy/ufunc.cpp"
+*/
+
+//#include "libs//python/src/object/class.cpp"
+#include "libs//python/src/object/enum.cpp"
+//#include "libs//python/src/object/function.cpp"
+#include "libs//python/src/object/function_doc_signature.cpp"
+//#include "libs//python/src/object/inheritance.cpp"
+//#include "libs//python/src/object/iterator.cpp"
+#include "libs//python/src/object/life_support.cpp"
+#include "libs//python/src/object/pickle_support.cpp"
+#include "libs//python/src/object/stl_iterator.cpp"
+
+
+// some .cpp files from the python folder are apparently not supposed to be included (what are they
+// good for?) so they are commented out

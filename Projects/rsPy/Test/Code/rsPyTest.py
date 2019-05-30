f4fa5117e98ba18b4ab1cb6d9c66a11fe3ee60f6
Robin Schmidt <robin@rs-met.com>
2019-05-30 15:53:06 +0200

    added rsPyTest stuff

---
+++ Projects/rsPy/Test/Code/rsPyTest.py
@@ -0,0 +1,32 @@
+﻿import rsPy as rs
+
+if __name__ == "__main__":
+    hi = rs.hello("Germany");
+    s  = rs.invite(hi)
+    print(s)
+    cnVal = rs.ellipj_cn(2.1231, 0.9) # 2nd argument must be in [0,1]
+    print(cnVal)
+
+
+
+
+"""
+
+from ctypes import windll
+import sys, os.path  
+
+if __name__ == "__main__":
+	scriptName = sys.argv[0]  
+	pathName = os.path.dirname(scriptName)  
+	dllFile = "rsPy.dll"  
+	locatedAt = (pathName + "/" + dllFile).replace("\\","/") 
+	dll = windll.LoadLibrary(locatedAt) # works, if the dll file is here
+	inviteFunc = dll.invite             # function is not found
+	inviteFunc()                        # call function from dll
+	print(dll)
+	# the dll is not found - maybe it looks in the wrong directory?
+
+"""
+
+# oh wait - it seems like this way of using a dll via ctypes is a different
+# way - we have a python extension that is used in a different way

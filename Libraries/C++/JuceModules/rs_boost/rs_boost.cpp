aa15d5c39c391cb507a259a563c57d6d3bb6f192
Robin Schmidt <robin@rs-met.com>
2019-05-31 22:16:34 +0200

    clean up

--- Libraries/C++/JuceModules/rs_boost/rs_boost.cpp
+++ Libraries/C++/JuceModules/rs_boost/rs_boost.cpp
@@ -8,7 +8,8 @@ header files that the compiler may be using. */
 
 #include "rs_boost.h"
 
-
+//=================================================================================================
+// boost::python
 
 #include "libs//python/src/dict.cpp"
 #include "libs//python/src/errors.cpp"
@@ -51,9 +52,6 @@ header files that the compiler may be using. */
 // https://stackoverflow.com/questions/32899621/numpy-capi-error-with-import-array-when-compiling-multiple-modules
 // https://docs.scipy.org/doc/numpy-1.10.1/reference/c-api.array.html#miscellaneous
 
-// ..but with these we still get linker errors for numpy, so for the time being, all numpy stuff
-// is commented out:
-
 #include "libs//python/src/numpy/dtype.cpp"
 #include "libs//python/src/numpy/matrix.cpp"
 #include "libs//python/src/numpy/ndarray.cpp"
@@ -61,52 +59,21 @@ header files that the compiler may be using. */
 #include "libs//python/src/numpy/scalars.cpp"
 #include "libs//python/src/numpy/ufunc.cpp"
 
-//BOOST_NUMPY_ARRAY_API = 0;
-//BOOST_UFUNC_ARRAY_API;
-void **PyArray_API = nullptr;   // ok - this fixes one of the linker errors
-//declared as extern in C:\Users\Rob\Anaconda3\Lib\site-packages\numpy\core\include\numpy\__multiarray_api.h, line 807
-
+void **PyArray_API = nullptr;
 void **PyUFunc_API = nullptr;
+// PyArray_API is declared as "extern" on line 807 in: 
+// Anaconda3\Lib\site-packages\numpy\core\include\numpy\__multiarray_api.h
+// and a similar declaratiion exists for PyUFunc_API. I guess, that we need to define them here but
+// they will be assigned by some initialization code within boost or python itself, but we need to
+// define the pointers here, or we'll get "unresolved external symbol" linker errors. here:
+// https://docs.scipy.org/doc/numpy-1.13.0/reference/c-api.ufunc.html#importing-the-api 
+// it says:
+// The C-API is actually an array of function pointers. This array is created (and pointed to by a 
+// global variable) by import_ufunc. The global variable is either statically defined or allowed to be 
+// seen by other files depending on the state of PY_UFUNC_UNIQUE_SYMBOL and NO_IMPORT_UFUNC.
 
-// hmm...these functions in question are marked as __declspec(dllimport) - so i guess that means
-// they are supposed to be dynamically linked...but maybe we nevertheless need to link to some
-// .lib file, see here: https://social.msdn.microsoft.com/Forums/vstudio/en-US/d94f6af3-e330-4962-a150-078da57ee5d0/error-lnk2019-unresolved-external-symbol-quotdeclspecdllimport-public-thiscall?forum=vcgeneral
-
-// maybe this one
-// C:\Users\Rob\Anaconda3\Lib\site-packages\numpy\core\lib\npymath.lib
-// or
-// C:\Users\Rob\Anaconda3\Lib\site-packages\numpy\core\*.pyd (for example: multiarray.pyd ...these are dlls)
-
-// this might be relevant, too:
+// some older links, collected while figuring out the linker issues - may not be relevant anymore:
+// https://social.msdn.microsoft.com/Forums/vstudio/en-US/d94f6af3-e330-4962-a150-078da57ee5d0/error-lnk2019-unresolved-external-symbol-quotdeclspecdllimport-public-thiscall?forum=vcgeneral
 // https://github.com/boostorg/python/issues/134
 // https://stackoverflow.com/questions/45069253/boost-numpy-linker-error-in-windows-vs
-
-// OR: maybe we need to set a macro, such that these functions will become marked as dllexport 
-// instead of import? yes - that seems to be the solution.
-
-// we still get 2 linker errors:
-// include_rs_boost.obj : error LNK2001 : unresolved external symbol BOOST_NUMPY_ARRAY_API
-// include_rs_boost.obj : error LNK2001 : unresolved external symbol BOOST_UFUNC_ARRAY_API
-
-// these are macros used in: 
-//   \boost\python\numpy\internal.hpp
-// and defined in:
-//   C:\Users\Rob\Anaconda3\Lib\site-packages\numpy\core\include\numpy\__multiarray_api.h and __ufunc_api.h
-// ...or are they define there? vs opens this file when click on "go to declaration"
-// but it jumps on PyObject_HEAD ...wtf? PyObject_HEAD itself is defined in
-// C:\Users\Rob\Anaconda3\include\object.h  as
-// /* PyObject_HEAD defines the initial segment of every PyObject. */
-// #define PyObject_HEAD                   PyObject ob_base;
-// but pointing with the mouse on BOOST_NUMPY_ARRAY_API, it says void** - so does this mean, the 
-// macro resolves to void**?
-
-// here's a reference for the numpy c-api - maybe there some ifo can be found, to which library
-// we are supposed to link:
 // https://docs.scipy.org/doc/numpy-1.13.0/reference/c-api.html
-
-
-
-
-
-
-
--- Libraries/C++/JuceModules/rs_boost/rs_boost.h
+++ Libraries/C++/JuceModules/rs_boost/rs_boost.h
@@ -35,48 +35,15 @@
 // what all these mt-gd-$&%§@ decorations mean, is explained here:
 //   https://www.boost.org/doc/libs/1_60_0/more/getting_started/windows.html#library-naming
 
-
 #define BOOST_PYTHON_SOURCE     // makes BOOST_PYTHON_DECL expand to __declspec(dllexport)
-#define BOOST_NUMPY_SOURCE      // test - seems to fix some (but not all) linker errors
+#define BOOST_NUMPY_SOURCE      // similar for numpy
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION  // gets rid of a warning
 
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION  // test - gets rid of a warning
-
-
-
-// some tests to get rid of the numpy linker errors
-//#define BOOST_PYTHON_NUMPY_INTERNAL  // test - doesn't help with numpy linker error
-//#define BOOST_PYTHON_NUMPY_INTERNAL  // test - nope!
-//#define _MULTIARRAYMODULE  // gives compile errors
-//#undef NO_IMPORT
-//#undef NO_IMPORT_ARRAY
-
-
-// defining these turns the numpy liker errors into compiler errors:
-//#define BOOST_NUMPY_ARRAY_API
-//#define BOOST_UFUNC_ARRAY_API
-// it seems they are defined as pointers-to-pointers-to-void? should they point to some global 
-// object? how is this supposed to work?
-
-// here: https://docs.scipy.org/doc/numpy-1.13.0/reference/c-api.ufunc.html#importing-the-api 
-// it says:
-// The C-API is actually an array of function pointers. This array is created (and pointed to by a 
-// global variable) by import_ufunc. The global variable is either statically defined or allowed to be 
-// seen by other files depending on the state of PY_UFUNC_UNIQUE_SYMBOL and NO_IMPORT_UFUNC.
-// ...maybe try compiling boost.python with boost's own build-system and see what that does
-
-
-//// should probably build a static library
-//#define BOOST_PYTHON_STATIC_LIB
+//#define BOOST_PYTHON_STATIC_LIB // my be used instead of .._SOURCE for building a static library?
 //#define BOOST_NUMPY_STATIC_LIB
 
-// aggregated header for all features:
 #include <boost/python.hpp>
 
-//// separate headers for specific features:
-//#include <boost/python/class.hpp>
-//#include <boost/python/module.hpp>
-//#include <boost/python/def.hpp>
-
-#include <iostream>
-#include <string>
+//#include <iostream>
+//#include <string>
 

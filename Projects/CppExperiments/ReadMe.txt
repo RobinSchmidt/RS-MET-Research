ad3eabd7a95a8f443ffafa62d25c98e7eacd24bf
Robin Schmidt <robin@rs-met.com>
2019-10-22 14:04:18 +0200

    added Logger

--- Projects/CppExperiments/ReadMe.txt
+++ Projects/CppExperiments/ReadMe.txt
@@ -1,3 +1,6 @@
-This project is meant to demonstrate various C++ constructs and design patterns for 
-copy/paste/edit. It can serve as quick reference to look up the syntax for a certain 
-construct.
+This project is meant to demonstrate various C++ constructs, idioms and design patterns for 
+copy/paste/edit into actual code. It can serve as quick reference to look up the syntax for a 
+certain construct and a place to grab the code from.
+
+todo:
+function pointers, lambdas, std::function, initializers, etc.
--- Projects/CppExperiments/Source/Main.cpp
+++ Projects/CppExperiments/Source/Main.cpp
@@ -4,15 +4,22 @@
 class SelfDeleter
 {
 public:
-  void selfDelete()
-  {
-    std::cout << "SelfDeleter::selfDelete";
-    // todo: figure out, how we can infer the funtion name - i think, there's a macro
-    delete this;
-  }
+  void selfDelete() { delete this; }
 };
 
 
+/** Logs the call of a function to std::cout. */
+class Logger
+{
+public:
+  void log()
+  {
+    std::cout << __func__ << " was called.\n";  // ISO C++
+    std::cout << __PRETTY_FUNCTION__ << "\n";  // only gcc
+    //std::cout << __FUNCSIG__  << "\n";       // only msc
+    //std::cout << "SelfDeleter::selfDelete";
+  }
+};
 
 int main()
 {
@@ -21,6 +28,9 @@ int main()
   sd->selfDelete();
   // todo: test, if this works - check for memory leaks and/or implement destructor and put
   // breakpoint ina dn see, if it gets hit
+
+  Logger logger;
+  logger.log();
 
 
   //std::cout << "Blah!";

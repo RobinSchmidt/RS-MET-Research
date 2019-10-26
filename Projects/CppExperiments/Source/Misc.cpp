4cdca224c639a4c06b3d9eed5d1ef6aa1cd90f19
Robin Schmidt <robin@rs-met.com>
2019-10-26 04:55:33 +0200

    added code

--- Projects/CppExperiments/Source/Misc.cpp
+++ Projects/CppExperiments/Source/Misc.cpp
@@ -6,6 +6,9 @@
 class Observee;  // forward declaration
 
 //-------------------------------------------------------------------------------------------------
+/** Baseclass for objects that need to keep track of the state of an Observee object. Subclasses 
+must implement the function observeeStateChanged and do whatever they need to do in response to
+that state change. */
 
 class AbstractObserver
 {
@@ -18,78 +21,99 @@ public:
 
   /** Callback function that must be overriden by subclasses to respond to changes of an Observee
   object. */
-  virtual void somethingChanged(Observee* o) = 0;
+  virtual void observeeStateChanged(Observee* o) = 0;
 
 };
 
 //-------------------------------------------------------------------------------------------------
+/** Class of objects whose state must be monitored by observer objects. */
 
 class Observee
 {
 
 public:
 
-  void addObserver(AbstractObserver* o) 
+  void registerObserver(AbstractObserver* o) 
   {  
     observers.push_back(o); // better: use some sort of appendIfNotThere function
   }
 
-  void removeObserver(AbstractObserver* o)
+  void deregisterObserver(AbstractObserver* o)
   {
     // removeValue(observers, o);
   }
 
+  /** Sends a notification to all our observers, that our state has changed. */
   void notifyObservers()
   {
-    for(auto& o : observers)
-      o->somethingChanged(this);
+    for(auto& o : observers)     // loop over all our observers
+      o->observeeStateChanged(this);
   }
 
+  /** Changes the state and notifies all our observers about the state change. */
   void changeSomething(int newValue)
   {
     something = newValue;
     notifyObservers();
   }
 
-  Observee(const std::string name) : _name(name) {} // demo facility
-  std::string getName() const { return _name; }     // demo facility
-  int getSomething() const { return something; }    // demo facility
-
-protected:
-
   std::vector<AbstractObserver*> observers;
 
-  std::string _name;  // demo facility
-  int something = 0;  // demo facility
+
+  // demo facilities:
+  Observee(const std::string name) : _name(name) {}
+  std::string getName() const { return _name; }
+  int getSomething() const { return something; }
+  std::string _name;
+  int something = 0;
 };
 
 //-------------------------------------------------------------------------------------------------
 
+/** Concrete subclass of AbstractObserver that responds to changes of the state of registered
+Observee objects by printing a message. */
+
 class ConcreteObserver : public AbstractObserver
 {
 
-  virtual void somethingChanged(Observee* o) override
+public:
+
+  virtual void observeeStateChanged(Observee* o) override
   {
     handleChange(o);
   }
 
   void handleChange(Observee* o)
   {
-    std::cout << typeid(*this).name() << "::" << __func__;
-    std::cout << " was called with observee " << o->getName() << ".\n";
-    std::cout << "The observee's new state is " << o->getSomething()<< ".\n\n";
+    std::cout << typeid(*this).name() << "::" << __func__ << " of object " << _name;
+    std::cout << " was called with observee " << o->getName();
+    std::cout << ". The observee's new state is " << o->getSomething()<< ".\n\n";
   }
-};
 
+  // demo facilities:
+  ConcreteObserver(const std::string name) : _name(name) {} 
+  std::string _name;
+};
 
 //-------------------------------------------------------------------------------------------------
 
 void demoObserver()
 {
   Observee observee("Foo");
-  ConcreteObserver observer;
-  observee.addObserver(&observer);
+
+  ConcreteObserver observer1("Bar");
+  ConcreteObserver observer2("Frob");
+
+  observee.registerObserver(&observer1);
+  observee.registerObserver(&observer2);
+
   observee.changeSomething(42);
+  observee.changeSomething(73);
 }
+
+// Variations:
+// -the Observer class can be defined inside the Observee class. For example, instead of having 
+//  classes Button and ButtonObserver, one could have Button and Button::Observer - that's actually
+//  nicer because it doesn't pollute the namespace so much.
 
 //=================================================================================================

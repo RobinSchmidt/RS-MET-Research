3ddd22d77e0771a38ba579d7ee349b01cded8d4d
Robin Schmidt <robin@rs-met.com>
2019-12-18 19:11:44 +0100

    added code to MultiArray

--- Projects/CppExperiments/Source/MultiArray.h
+++ Projects/CppExperiments/Source/MultiArray.h
@@ -12,14 +12,29 @@ public:
   {
     // todo: allocate memory, update strides
 
+    int rank = (int) shape.size();
+    strides.resize(rank);
 
-    int dummy = 0;
+    // update strides (needs test):
+    int i = rank-1;
+    int s = 1;
+    while(i >= 0) {
+      strides[i] = s;
+      s *= shape[i];
+      --i;
+    }
+
+    // compute required space and allocate memory:
+    s = 1;
+    for(i = 0; i < rank; i++)
+      s *= shape[i];
+    data.resize(s);
   }
 
   template<typename... Rest>
   T& operator()(int i, Rest... rest) 
   {
-    return &data(flatIndex((int)shape.size()-1, i, rest...));
+    return data[ flatIndex((int)shape.size()-1, i, rest...) ];
   }
   // goal: we want to be able to use the operator like (i), (i,k), (i,j,k), ...
 
@@ -51,8 +66,11 @@ public:
 
 protected:
 
+  // factor out inot class MultiArrayView:
   std::vector<int> shape;
   std::vector<int> strides;
+
+  // MultiArray itself should only have this additional member
   std::vector<T>   data;
 
 };
@@ -64,6 +82,9 @@ void testMultiArray()
   typedef MultiArray<float> MA;
 
   MA a1 = MA(VecI{3});  // should create a 3D vector
+  a1(0) = 1;
+  a1(1) = 2;
+  a1(2) = 3;
 
 
   int dummy = 0;

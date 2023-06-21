df1905cf5ebace97ff42c7811fefedbfae51f299
Robin Schmidt <robin@rs-met.com>
2023-06-21 19:27:19 +0200

    Update Experiments.cpp

--- Prototypes/Cpp/Source/Experiments.cpp
+++ Prototypes/Cpp/Source/Experiments.cpp
@@ -11933,8 +11933,9 @@ void testNumericPotential()
   // Add the B,A coeffs:
   for(int k = 0; k < J; k++)
   {
-    R(k, k)   = B;
-    R(k, k+J) = A;
+    R(k,     k)     = B;
+    R(k,     k+J)   = A;
+    R(N-1-k, N-1-k) = A;
   }
 
 

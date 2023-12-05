9e5434f616901cfbd1210d04d53b6c06e4914db9
Robin Schmidt <robin@rs-met.com>
2023-12-05 23:05:01 +0100

    Update Experiments.cpp

--- Prototypes/Cpp/Source/Experiments.cpp
+++ Prototypes/Cpp/Source/Experiments.cpp
@@ -10599,6 +10599,12 @@ void testSylvesterMatrix()
 
   // ...TBC...
 
+  // Another example:
+  f.setCoeffs({ -5, -1, -3, -2, +3, +1 }); // f = x^5 + 3*x^4 - 2*x^3 - 3*x^2 - 1*x - 5
+  g.setCoeffs({ -8, +1, +1, -5, +7, +1 }); // g = x^5 + 7*x^4 - 5*x^3 + 1*x^2 + 1*x - 8
+  S = rsSylvesterMatrix(f, g); // raises assert
+
+
 
 
   int dummy = 0;

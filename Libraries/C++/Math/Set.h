bc808098ee9a5c381f922f1461204b91bed0d893
Robin Schmidt <robin@rs-met.com>
2024-05-26 20:51:50 +0200

    Update Set.h

---
+++ Libraries/C++/Math/Set.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace rema  // Rob's educational math algorithms
+{
+
+}
+
+// remo Rob's educational math objects
+// remu                        utilities
+// remi                        implementations
+//    p                        prototypes
+//    a                        algorithms

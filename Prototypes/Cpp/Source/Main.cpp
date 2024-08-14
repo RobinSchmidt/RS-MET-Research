cfa712e41ee8f7b1c69e7a591be11ffa936c2a5d
Robin Schmidt <robin@rs-met.com>
2024-08-14 23:23:10 +0200

    added text

--- Prototypes/Cpp/Source/Main.cpp
+++ Prototypes/Cpp/Source/Main.cpp
@@ -88,7 +88,7 @@ int main (int /*argc*/, char* /*argv[]*/)
   //testNeumannRationals();
   //testPairingFunctions();
 
-  //testGeneralizedCollatz();
+  testGeneralizedCollatz();
   //testPowerCommutator();
   //testParticleSystem();
   //testWeightedAverages();

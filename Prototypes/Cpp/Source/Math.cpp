00781a8437f5d8346cbd12e567a72d4bcb0b2ec4
Robin Schmidt <robin@rs-met.com>
2026-01-15 15:55:56 +0100

    Extend specification of rsPolynomialRootFinder

--- Prototypes/Cpp/Source/Math.cpp
+++ Prototypes/Cpp/Source/Math.cpp
@@ -44,6 +44,63 @@ protected:
 // - Maybe try to implement this class with the help from Copilot. It seems to be a suitable task
 //   to tackle with AI assistance because it has a well defined and managable scope without being
 //   too trivial. The AI probably knows the algorithms better than what I could figure out.
+//
+// - Try to avoid a direct dependency on RAPT::rsPolynomial. Maybe it should work on raw C-arrays
+//   of coeffs and roots. It should also work well together with boost::polynomial:
+//   https://www.boost.org/doc/libs/1_64_0/libs/math/doc/html/math_toolkit/roots/polynomials.html
+//   Maybe we should have a convenience class that works directly with boost::polynomial and a raw
+//   number crunching class that works on raw arrays. 
+// 
+// - For the roots, it should be convenient to use TArg = std::complex<TCoef> when TCoef is float 
+//   or double or long double but it should also be possible to use selfmade complex number classes
+//   such as RAPT::rsComplex. Maybe, the low level API can assume that arrays of complex numbers 
+//   can be interpreted as arrays of adjacent pairs of real numbers as std::complex defines it:
+//   https://en.cppreference.com/w/cpp/numeric/complex.html
+//   This would put some constraints on hwo the complex number class has to be implemented, though.
+//   But the constraints seem to be reasonable. It's quite common to store arrays complex numbers 
+//   that way. For example, many FFT libraries assume that a length N array of complex numbers is
+//   stored as a length 2*N array of real numbers (i.e. of type float or double). 
+// 
+// - It would, however, be a nice to have if we can avoid making any assumptions about the 
+//   representation and memory layout of complex numbers. If we really try to avoid that 
+//   constraint, then maybe test it with a complex number class that has real and imaginary part 
+//   swapped or stores the numbers in polar form.
+//
+// - Assume that "Real" stands for float, double or long double. Then, the most important use cases
+//   for the root finder are as follows (most important on top):
+// 
+//     - TCoeff = std::complex<Real>, TArg = std::Complex<Real>
+//     - TCoeff = Real, TArg = std::Complex<Real>
+//     - TCoeff = Real, TArg = Real
+// 
+// - It should also be easy to replace std::complex with rsComplex or some other implementation of
+//   complex numbers. In these other implementations, it should be possible use use for Real 
+//   something like rsFloat32x2, rsFloat64x2, rsSimsVector<float, 4>, std::simd, see:
+//   https://en.cppreference.com/w/cpp/experimental/simd.html
+// 
+// - It should also be possible to use it with extended precision libraries for the Real type.
+//
+// - It would be nice if the class would be general enough such that it could also handle 
+//   polynomials with coefficients and/or roots that are: rational, integer, from a finite field
+//   (aka Galois field). It would also be nice if we could use matrices for the coefficients and/or
+//   the arguments. Maybe multivectors?
+//
+// - The problem is: Depending of the data types for TCoef and TArg, we may have to use completely
+//   different algorithms for finding the roots. In a finite field, if it isn't too big, we could
+//   just try all possible values. ToDo: Figure out if there is a better algorithm to find such 
+//   roots. For finding real and rational roots, we could perhaps use the algorithms to find real
+//   roots and then use a round-and-verify step. For "rounding" real number to rational numbers,
+//   maybe we could use continued fraction expansions or Farey sequences (the latter was 
+//   auto-completed by GitHub Copilot - I have no idea, if this makes sense). 
+//
+// - The root finding for discrete number types (integer, rational, Galois) need not to be 
+//   implemented right from the start but the API should designed in such a way that these things 
+//   can easily be added later in a way that feels consistent with the root-finding for real and 
+//   complex numbers. We may need to think about extension fields in this context. In general, the
+//   roots (i.e. TArg) will be an extension field of TCoef. For example, the complex numbers are an
+//   extension field of the real numbers.
+
+
 
 
 
